b71e90a0f3e09e7bbac9f596ece38f1bc5a47819
lassr1000 <162852385+202410825@users.noreply.github.com>
2025-12-01 11:19:04 +0900

    Add files via upload

---
+++ Homework9.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Homework9 {
+    public static void main(String[] args){
+
+        Map<String, String> loginMap = new HashMap<>();
+
+        try {
+            Scanner fileScanner = new Scanner(new File("db.txt"));
+
+            while(fileScanner.hasNext()){
+                String id = fileScanner.next();
+                String pw = fileScanner.next();
+                loginMap.put(id, pw);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("No file");
+            return;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("id와 password를 입력해주세요.");
+
+            System.out.print("id: ");
+            String id = scanner.nextLine().trim();
+
+            if(!loginMap.containsKey(id)){
+                System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요.");
+                continue;
+            }
+
+            System.out.print("password: ");
+            String pw = scanner.nextLine().trim();
+
+            if(!loginMap.get(id).equals(pw)){
+                System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+            }
+            else{
+                System.out.println("id와 비밀번호가 일치합니다.");
+                break;
+            }
+
+            scanner.close();
+        }
+    }
+}
